d1d843e0e5afe4d8e4e9afe5545f1e79fff63113
prasanthsankar32 <prasanthsankar32@gmail.com>
2019-07-27 14:55:27 +0530

    Updates

---
+++ precache-manifest.fd188307c3767a487c071cc3f944413d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9829d836a62258aae2a5b3aaab7ba11c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "655bad43e4b0c41a93a2",
+    "url": "/portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/portfolio/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "655bad43e4b0c41a93a2",
+    "url": "/portfolio/static/js/main.caa2b61b.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
